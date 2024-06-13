2fb851a18bf73192f4b3442c76f6640ab7a8edbf
OscarSebastianRuedaRojas <152028425+OscarSebastianRuedaRojas@users.noreply.github.com>
2024-06-12 21:54:55 -0500

    feat: :sparkles: Creacion metodos equipo
    Se crearon los respectivos metodos del Controler de equipo
    
    @trainingLeader

--- src/main/java/com/betplay2/Main.java
+++ src/main/java/com/betplay2/Main.java
@@ -1,7 +1,60 @@
 package com.betplay2;
 
+import java.util.Scanner;
+
+import com.betplay2.controllers.EquipoController;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        EquipoController equipos = new EquipoController();
+        Boolean seguir = true;
+        while (seguir) {
+            switch (menuPrincipal(input)) {
+                case 1:
+                    while (true) {
+                        if (equipos.create(input)) {
+                            continue;
+                        }else{
+                            break;
+                        }
+                    }
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                case 5:
+                    
+                    break;
+                case 6:
+                    seguir = false;
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+        
+        
+        input.close();
+    }
+    public static int menuPrincipal(Scanner input){
+        System.out.println("Bienvenido a la Liga Betplay");
+        System.out.println("1. Agregar equipo");
+        System.out.println("2. Agregar partido");
+        System.out.println("3. Reportes");
+        System.out.println("4. Tabla de posiciones");
+        System.out.println("5. Añadir personal");
+        System.out.println("6. Salir");
+        System.out.print("Seleccione una opción: ");
+        int responder = input.nextInt();
+        input.nextLine();
+        return responder;
     }
 }
---
+++ src/main/java/com/betplay2/controllers/EquipoController.java
@@ -0,0 +1,33 @@
+package com.betplay2.controllers;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+import com.betplay2.models.Create;
+import com.betplay2.models.Equipo;
+
+
+public class EquipoController implements Create<Boolean>{
+    ArrayList<Equipo> equipos = new ArrayList<>();
+    @Override
+    public Boolean create(Scanner input) {
+        Equipo equipo = new Equipo();
+        System.out.println("Ingresa el nombre del equipo que deseas crear");
+        equipo.setNombre(input.nextLine());
+        equipos.add(equipo);
+        System.out.println(String.format("El equipo %s fue agregado exitosamente", equipo.getNombre()));
+        System.out.println("Desea agregar otro equipo? \n 1.Si \n 2.No");
+        int respuesta = input.nextInt();
+        input.nextLine();
+        if (respuesta==1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public ArrayList<Equipo> copiaArrayList(){
+        ArrayList<Equipo> copia = new ArrayList<>();
+        copia.addAll(equipos);
+        return copia;
+    }
+}
